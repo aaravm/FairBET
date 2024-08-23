419e94b00cebab6aab24d76dcf8781952168e311
lla-dane <abhinavagarwalla6@gmail.com>
2024-08-24 00:21:20 +0530

    NILLION INFRA QUICKSTART

---
+++ nillion-python-starter/quickstart/nada_quickstart_programs/src/addition.py
@@ -0,0 +1,11 @@
+from nada_dsl import *
+
+def nada_main():
+    GAME_MANAGER = Party(name="PLAYER")
+    PLAYER = Party(name="PLAYER")
+    SECRET1 = SecretInteger(Input(name="SECRET1", party=PLAYER))
+    SECRET2 = SecretInteger(Input(name="SECRET2", party=PLAYER))
+    
+    RESULT = SECRET1 + SECRET2 
+    
+    return [Output(RESULT, "RESULT", GAME_MANAGER)]
