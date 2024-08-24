521fb94b688b74771cd30d1c8594beec64507e39
lla-dane <abhinavagarwalla6@gmail.com>
2024-08-25 00:05:47 +0530

    started game-manager client support

--- nillion-python-starter/quickstart/client_code/player_client.py
+++ nillion-python-starter/quickstart/client_code/player_client.py
@@ -26,6 +26,7 @@ from cosmpy.crypto.keypairs import PrivateKey
 home = os.getenv("HOME")
 load_dotenv(f"{home}/.config/nillion/nillion-devnet.env")
 
+# FIXME: GET THIS FUNCTION IN A UTILS FILE 
 def string_to_int(s):
     hash_object = hashlib.sha256(s.encode())    # USE SHA256 HASH FUNCTION
     hex_dig = hash_object.hexdigest()           # GET HEXADECIMAL DIGEST
@@ -91,30 +92,24 @@ def get_system_info():
 
 async def main():
     
-    # GET THE NECESSARY SYSTEM IDENTIFIERS
-    system_info = get_system_info()
-    serial_number = system_info['serial_number']
-    os_id = system_info['os']
-    
-    sn_int = string_to_int(serial_number)
-    os_int = string_to_int(os_id)
-    
     # GET NILLION-DEVNET CREDENTIALS
     cluster_id = os.getenv("NILLION_CLUSTER_ID")
     grpc_endpoint = os.getenv("NILLION_NILCHAIN_GRPC")
     chain_id = os.getenv("NILLION_NILCHAIN_CHAIN_ID")
 
 
-    # INITIATE A NILLION CLIENT USING THE SEED
-    seed = "my_seed"
+    # INITIATE A NILLION CLIENT USING THE SEED ********
+    seed = "PLAYER"
     userkey = UserKey.from_seed(seed)
-    nodekey = NodeKey.from_seed(seed)
+    node_key = NodeKey.from_seed(seed)
 
-    player = create_nillion_client(userkey, nodekey)
+    player = create_nillion_client(userkey, node_key)
 
+    # THIS IS A peerId FOR A CLIENT IN THE NILLION NET, WHILE GIVING COMPUTE PERMISSIONS THIS IS USED 
     party_id = player.party_id
+    # THIS IS DERIVED FROM THE USER'S PYBLIC KEY ADN CAN BE USED AS A PUBLIC USER IDENTIFIER
     user_id = player.user_id
-    
+        
     # HOW WILL BE STORE THE COMPUTATION PROGRAMS ??
     # WE CREATE DIFFERENT COMPUATION PROGRAMS AND REFER TO THEIR .nada.bin FILE.
     # DEPLOY ALL THE COMPUTATIONS IN DEVNET
@@ -130,7 +125,7 @@ async def main():
         prefix="nillion",
     )
     
-    # PAY FOR DEPLOYMENT
+    # PAY FOR DEPLOYMENT   *** ALL THE PROGRAMS SHOULD BE DEPLOYED BY THE GAME_MANAGER
     receipt_store_program = await get_quote_and_pay(
         player,
         nillion.Operation.store_program(get_sn_path),
@@ -148,20 +143,34 @@ async def main():
     program_id = f"{user_id}/{get_sn}"
     print("PROGRAM STORED -> ID:", program_id)
     
+    # GET ALL THE NECESSARY USER CREDENTIALS WHICH CAN BE USED FOR HARWARE BAN: SERIAL NUMBER, PUBLIC KEY 
+    
+    # GET THE NECESSARY SYSTEM IDENTIFIERS
+    system_info = get_system_info()
+    serial_number = system_info['serial_number']
+    
+    pub_key_int = string_to_int(user_id)
+    sn_int = string_to_int(serial_number)
     
     # SECRET INSTANCES CREATION
     NEW_SECRETS = nillion.NadaValues(
         {
             "SECRET1": nillion.SecretInteger(sn_int),
-            "SECRET2": nillion.SecretInteger(os_int)
+            "SECRET2": nillion.SecretInteger(20)
         }
     )
     
-    GAME_MANAGER_PARTY = "PLAYER"
+    # PARTY NAMES WE NEED SO TO SPECIFY WHICH CLIENT IS GIVING THE INPUTS AND RECEIVING THE OUTPUTS
+    GAME_MANAGER_PARTY = "PLAYER" # *******
     PLAYER_PARTY = "PLAYER"
     
-    # SET THE PERMISSIONS TO ALLOW THE USER TO PERFORM COMPUTATION ON THE SECRETS FOR THE SPECIIFIED PROGRAM_ID
+    # CREATES A PREMISSION OBJECT TO ATTACH TO THE STORED SECRET
+    
+    # DEFAULT PERMISSIONS ALLOWS TO USER TO UPDATE AND RETRIEVE THE SECRET 
     permissions = nillion.Permissions.default_for_user(player.user_id)
+    
+    # COMPUTE PERMISSIONS ALLOWS THE USER TO USE THE SECRET AS AN INPUT FOR THE COMPUTATIONS IN THE SPECIFIED PROGRAM.
+    # USER_ID AND PROGRAM_ID
     permissions.add_compute_permissions({player.user_id: {program_id}})
 
     # PAY T0 DEPLOYMENT OF THE SECRET
@@ -212,13 +221,15 @@ async def main():
         COMPUTE_SECRETS,         
         receipt_compute,
     )
+    
+    
+    # THE BELOW CODE IS RESUNDANT, WE DONT NEED IT
 
     # RETURN THE COMPUTATION RESULTS
     print(f"THE COMPUTATION WAS SENT THE NETWORK -> ID: {compute_id}")
     while True:
         compute_event = await player.next_compute_event()
         if isinstance(compute_event, nillion.ComputeFinishedEvent):
-            # print(f"✅  Compute complete for compute_id {compute_event.uuid}")
             print(f"🖥️  THE UNIFIED SYSTEM IDENTIFIER FOR HARDWARE BAN {compute_event.result.value}")
             return compute_event.result.value
 
